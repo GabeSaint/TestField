7fc3407b276c571ce25b1c4a76c9cca962977dad
GabeSaint <38449112+GabeSaint@users.noreply.github.com>
2019-06-14 15:08:52 -0700

    Update newest.js

--- newest.js
+++ newest.js
@@ -1,3 +1,9 @@
+  window.onload=function(){
+    document.getElementById("buttonFee").style.display='none';
+}
+
+
+
 $.ajax({
   url: 'https://raw.githubusercontent.com/GabeSaint/TestField/master/tuitionsample.json',
   dataType: 'json',
@@ -7,13 +13,21 @@ $.ajax({
       var option1 = $('<option id="' + val.YearVal + '">' + val.Year + '</option>');
       $('#Year').append(option1);
       var option2 = $('<option id="' + val.StudentStatVal + '">' + val.StudentStat + '</option>');
+      if (val.StudentStat == ""){
+        // Do nothing
+      } else {
       $('#StudentStat').append(option2);
+    }
       var option3 = $('<option id="' + val.LocationVal + '">' + val.Location + '</option>');
       $('#Location').append(option3);
       var option4 = $('<option id="' + val.ResidencyVal + '">' + val.Residency + '</option>');
+      if (val.Residency == ""){
+        // Do nothing
+      } else {
       $('#ResidencyStat').append(option4);
-      var option5 = $('<option id="' + val.AcademicVal + '">' + val.Academic + '</option>');
-      $('#Academic').append(option5);
+    }
+      /*var option5 = $('<option id="' + val.AcademicVal + '">' + val.Academic + '</option>');
+      $('#Academic').append(option5);*/
     })
   },
   error: function() {
@@ -26,10 +40,18 @@ $.ajax({
   }
 })
 
+function showButton()
+{
+  document.getElementById("buttonFee").style.display='block';
+
+  }
+
+
 function showInfo()
   {
     //clear previous results
     $('#result1').html('');
+    $('#result2').html('');
 
 
     //get the id of the select
@@ -37,11 +59,11 @@ function showInfo()
     var StudentStatid = $('#StudentStat').find('option:selected').attr('id');
     var Locationid = $('#Location').find('option:selected').attr('id');
     var ResidencyStatid = $('#ResidencyStat').find('option:selected').attr('id');
-    var Academicid = $('#Academic').find('option:selected').attr('id');
+    /*var Academicid = $('#Academic').find('option:selected').attr('id');*/
     var Honorsid= $('input[type=checkbox]').prop('checked');
 
     // Create the ID
-    var Totalid = Yearid + '-' + StudentStatid + '-' + Locationid + '-' + ResidencyStatid + '-' + Academicid
+    var Totalid = Yearid + '-' + StudentStatid + '-' + Locationid + '-' + ResidencyStatid// + '-' + Academicid
     //document.getElementById("testValue").innerHTML = Totalid + '-' + Honorsid; // Test line above
 
     // Call Function - Display Table
@@ -52,6 +74,7 @@ function showInfo()
   {
     // Data placement
     $('#result1').append("<div></div>");
+    $('#result2').append("<div></div>");
 
     $.getJSON('https://raw.githubusercontent.com/GabeSaint/TestField/master/tuitionsample.json', function(data)
     {
@@ -68,8 +91,9 @@ function showInfo()
           val["Fee Total"] +
           '</td><td>' +
           val["Grand total"] +
-          '</td></tr></table>&nbsp;<br>&nbsp;' +
-          '<table><tr><th class="FTH">FA<br>Trust</th><th class="FTH">Health<br>&amp; Rec</th><th class="FTH">Activity</th><th class="FTH">Info<br>Tech</th><th class="FTH">Green</th><th class="FTH">ASNAU</th><th class="FTH">Athletics</th><th class="FTH">Fee<br>Total</th></tr><tr><td>' +
+          '</td></tr></table>&nbsp;<br>&nbsp;';
+
+          var tableResultFees = '<table><tr><th class="FTH">FA<br>Trust</th><th class="FTH">Health<br>&amp; Rec</th><th class="FTH">Activity</th><th class="FTH">Info<br>Tech</th><th class="FTH">Green</th><th class="FTH">ASNAU</th><th class="FTH">Athletics</th><th class="FTH">Fee<br>Total</th></tr><tr><td>' +
           val["FA Trust"] +
           '</td><td>' +
           val["Health/Rec"] +
@@ -87,6 +111,7 @@ function showInfo()
           val["Fee Total"] +
           '</td></tr></table>';
           $('#result1').append(tableResult);
+          $('#result2').append(tableResultFees);
           $(".FTH").css({"background-color":"#FFD200", "font-size":"16px", "border-spacing":"0px" , "border":"0px solid #cccccc", "text-align":"center", "font-weight":"bold" });
           $("table").css({"border-spacing":"0px" , "border":"0px solid #cccccc", "text-align":"center" });
           $("td").css({"border-spacing":"0px" , "border":"1px solid #cccccc", "text-align":"center"});
@@ -104,8 +129,9 @@ function showInfo()
           val["Fee Total"] +
           '</td><td>' +
           val["Grand total"] +
-          '</td></tr></table>&nbsp;<br>&nbsp;' +
-          '<table><tr><th class="FTH">FA<br>Trust</th><th class="FTH">Health<br>&amp; Rec</th><th class="FTH">Activity</th><th class="FTH">Info<br>Tech</th><th class="FTH">Green</th><th class="FTH">ASNAU</th><th class="FTH">Athletics</th><th class="FTH">Fee<br>Total</th></tr><tr><td>' +
+          '</td></tr></table>&nbsp;<br>&nbsp;';
+
+          var tableResultFees = '<table><tr><th class="FTH">FA<br>Trust</th><th class="FTH">Health<br>&amp; Rec</th><th class="FTH">Activity</th><th class="FTH">Info<br>Tech</th><th class="FTH">Green</th><th class="FTH">ASNAU</th><th class="FTH">Athletics</th><th class="FTH">Fee<br>Total</th></tr><tr><td>' +
           val["FA Trust"] +
           '</td><td>' +
           val["Health/Rec"] +
@@ -123,6 +149,7 @@ function showInfo()
           val["Fee Total"] +
           '</td></tr></table>';
           $('#result1').append(tableResult);
+          $('#result2').append(tableResultFees);
           $(".FTH").css({"background-color":"#FFD200", "font-size":"16px", "border-spacing":"0px" , "border":"0px solid #cccccc", "text-align":"center", "font-weight":"bold" });
           $("table").css({"border-spacing":"0px" , "border":"0px solid #cccccc", "text-align":"center" });
           $("td").css({"border-spacing":"0px" , "border":"1px solid #cccccc", "text-align":"center"});
@@ -132,3 +159,13 @@ function showInfo()
 
     });
   }
+
+  function toggleFees() {
+  var x = document.getElementById("tuitionFeesDisplay");
+  document.getElementById("result2").style.display='block';
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
