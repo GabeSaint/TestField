f34efff4dfb59c5d4bdc515154531ccca87bf094
GabeSaint <38449112+GabeSaint@users.noreply.github.com>
2019-07-03 08:11:42 -0700

    Update newest.js

--- newest.js
+++ newest.js
@@ -218,7 +218,7 @@ function tuitionTable(Totalid, Honorsid)
         document.getElementById("errorMessage").style.display='none';
       }
 
-      else if (Totalid !== val["TuitionIdentifier"] && (Academicid && StudentStatid && Locationid && ResidencyStatid) !== defaultDDval  ) {
+      else if (Totalid !== val["TuitionIdentifier"] && (Yearid && StudentStatid && Locationid && ResidencyStatid) !== defaultDDval  ) {
         var errorResponse = "You done GOOFED";
         $('#errorMessage').append(errorResponse);
 
