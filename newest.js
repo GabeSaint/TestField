4030ed8287be1946bf4a9f7d04992036a8a8aca4
GabeSaint <38449112+GabeSaint@users.noreply.github.com>
2019-07-08 16:45:51 -0700

    Update newest.js

--- newest.js
+++ newest.js
@@ -77,7 +77,7 @@ function showInfo()
   var Totalid = Yearid + '-' + StudentStatid + '-' + Locationid + '-ONLINE';
   document.getElementById("ResidencyStat").style.display='none';
   document.getElementById("ResidencyStat2").style.display='none';
-  document.getElementById('buffer').innerHTML = "<br> <br> <br>"
+  document.getElementById('buffer').innerHTML = "<br> <br> <br>";
   } else{ var Totalid = Yearid + '-' + StudentStatid + '-' + Locationid + '-' + ResidencyStatid;
   document.getElementById("ResidencyStat").style.display='inline';
   document.getElementById("ResidencyStat2").style.display='inline';
@@ -103,8 +103,32 @@ if (Locationid == "SWC"){
   Totalid = Yearid + '-' + StudentStatid + '-' + Locationid + '-' + ResidencyStatid;
 }
 
+//here I was
+if (Yearid == "S19" || Yearid == "W19"){
+document.getElementById("Honors").style.display='none';
+document.getElementById("Honors2").style.display='none';
+document.getElementById("ResidencyStat").style.display='none';
+document.getElementById("ResidencyStat2").style.display='none';
+document.getElementById('buffer').innerHTML = "<br> <br> <br>";
+Totalid = Yearid + '-' + StudentStatid + '-' + Locationid;
+} else {
+document.getElementById("Honors").style.display='inline';
+document.getElementById("Honors2").style.display='inline';
+//document.getElementById("ResidencyStat").style.display='inline';
+//document.getElementById("ResidencyStat2").style.display='inline';
+document.getElementById('buffer').style.display='none';
+
+  if (Locationid == "ONLINE" ){
+    document.getElementById("Honors").style.display='none';
+    document.getElementById("Honors2").style.display='none';
+  var Totalid = Yearid + '-' + StudentStatid + '-' + Locationid + '-ONLINE';}
+  else{
+    Totalid = Yearid + '-' + StudentStatid + '-' + Locationid + '-' + ResidencyStatid;
+  }
+}
+
 // Call Function - Display Table
-tuitionTable(Totalid, Honorsid);
+tuitionTable(Totalid, Honorsid, Yearid);
 
 }
 
@@ -118,6 +142,45 @@ function tuitionTable(Totalid, Honorsid, Yearid, StudentStatid, Locationid, Resi
   {
     $.each(data, function(key, val)
     {
+      if (Yearid == "S19" || Yearid == "W19") {
+        if (Totalid == val["TuitionIdentifier"]) {
+
+         var tableResult = '<h2>Tuition &amp; Fees</h2><table><tr><th class="FTH">Fees</th><th class="FTH">Tuition</th> <th class="FTH">Total</th></tr><tr><td>' +
+           val["Fee Total-F"] +
+         '</td><td>' +
+           val["Tuition-F"] +
+         '</td><td>' +
+      val["Grand total"] +
+    '</td></tr></table>';
+
+         var tableResultFees = '<table><tr><th class="FTH">FA<br>Trust</th><th class="FTH">Health<br>&amp; Rec</th><th class="FTH">Info<br>Tech</th><th class="FTH">Green</th><th class="FTH">Fee<br>Total</th></tr><tr><td>' +
+           val["FA Trust-F"] +
+         '</td><td>' +
+      val["Health/Rec-F"] +
+    '</td><td>' +
+      val["Info Tech-F"] +
+    '</td><td>' +
+      val["Green-F"] +
+    '</td><td>'+
+      val["Fee Total-F"]+
+    '</td></tr></table>'+
+    "<p><a title='Program Fees' href='https://stage.wp.nau.edu/pubops-testing-sandbox/program-fees/'>View our Program Fees</a></p>";
+
+         $('#result1').append(tableResult);
+         $('#result2').append(tableResultFees);
+         $(".FTH").css({"background-color":"#FFD200", "font-size":"16px", "border-spacing":"0px" , "border":"0px solid #cccccc", "text-align":"center", "font-weight":"bold" });
+         $("table").css({"border-spacing":"0px" , "border":"0px solid #cccccc", "text-align":"center" });
+         $("td").css({"border-spacing":"0px" , "border":"1px solid #cccccc", "text-align":"center"});
+         $("button#buttonFee").css({"background-color":"#ffd200" , "color":"black"});
+         showButton(Totalid);
+         scrollWin();
+
+       }
+
+
+      }else {
+
+
       // Check the Degree Code
       if(Totalid == val["TuitionIdentifier"] && Honorsid === false)
       {
@@ -214,8 +277,7 @@ function tuitionTable(Totalid, Honorsid, Yearid, StudentStatid, Locationid, Resi
         showButton(Totalid);
         scrollWin();
       }
-
-      
+    }
     });
 
   });
