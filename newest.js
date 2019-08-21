0c33b388a662021b3a170422292e9ef4db63ba5f
Gabriel J Santos <gjs88@iris.nau.edu>
2019-08-21 14:31:36 -0700

    Attempt 2 for the $ sign

--- newest.js
+++ newest.js
@@ -440,14 +440,14 @@ function tuitionTable(Totalid, Honorsid, Yearid, StudentStatid, Locationid, Resi
     val["Tuition-F"] +
     '</td><td>' +
     val["Fee Total-F"] +
-    '</td><td>' +
-    $+subTotalsF +
+    '</td><td>&dollar;' +
+    subTotalsF +
     '</td></tr><tr><td class="emptyCell">Spring 2019</td><td>' +
     val["Tuition-S"] +
     '</td><td>' +
     val["Fee Total-S"] +
-    '</td><td>' +
-    $+subTotalsS +
+    '</td><td>&dollar;' +
+    subTotalsS +
     '</td></tr><tr><td class="emptyCell"></td><td class="emptyCell"></td><td class="emptyCell">Total</td><td>' +
     val["Grand total"] +
     '</td></tr></table>';
